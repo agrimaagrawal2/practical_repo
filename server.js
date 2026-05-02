6eb596e52c4e936bcd31fbc55d8e48f9ed25554f
agrimaagrawal2 <agrima.agrawal_cs23@gla.ac.in>
2026-05-02 14:54:59 +0530

    file practical

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+
+const connectDB = require("./config/db");
+connectDB();
+
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
